b475809d6fa49f913ba852335c39e4307eedb0f9
NoorAzar11 <nooraaz945@gmail.com>
2021-03-31 15:49:14 +0300

    add Q1 function

--- js/app.js
+++ js/app.js
@@ -4,80 +4,86 @@
 
 let userName = prompt('tab your name please ');
 // console.log(userName);
-alert('WELCOME,let me Introdus myself Mr / Mrs '+ userName )
+alert('WELCOME,let me Introdus myself Mr / Mrs ' + userName)
 
 // guessing game question
 
-alert ('first of all let me ask you a few questions about me to see if u will have correct answers.')
+alert('first of all let me ask you a few questions about me to see if u will have correct answers.')
 
 // first question 
-let score = 0;
+function firstQuestion() {
 
-let myAge = prompt("do you think i'm older than 28 ?" );
-if(myAge.toLowerCase() == "yes" || myAge.toLowerCase() == "y"){
-    alert("Correct");
-    score++
-    // console.log(myAge);
-}else if(myAge.toLowerCase() == "no" || myAge.toLowerCase() == "n"){
-    alert("Wrong");
-    // console.log(myAge);
-}else{
-    alert("Enter only yes ' y or no ' n .");
+
+    let score = 0;
+
+    let myAge = prompt("do you think i'm older than 28 ?");
+    if (myAge.toLowerCase() == "yes" || myAge.toLowerCase() == "y") {
+        alert("Correct");
+        score++
+        // console.log(myAge);
+    } else if (myAge.toLowerCase() == "no" || myAge.toLowerCase() == "n") {
+        alert("Wrong");
+        // console.log(myAge);
+    } else {
+        alert("Enter only yes ' y or no ' n .");
+    }
 }
+firstQuestion();
+
 // second question 
 
 let myStudy = prompt('do i study in the same Field ? ');
-if(myStudy.toLowerCase() == "yes" || myStudy.toLowerCase() == "y"){
+if (myStudy.toLowerCase() == "yes" || myStudy.toLowerCase() == "y") {
     alert("good answer");
     score++
 
     // console.log(myStudy);
-}else if(myStudy.toLowerCase() == "no" || myStudy.toLowerCase() == "n"){
+} else if (myStudy.toLowerCase() == "no" || myStudy.toLowerCase() == "n") {
     alert("oops");
     // console.log(myStudy);
-}else{
+} else {
     alert("Enter only yes ' y or no ' n .");
 }
 // third question 
 
 let myJob = prompt('do i have work in programming befor ?');
-if(myJob.toLowerCase() == "yes" || myJob.toLowerCase() == "y"){
+if (myJob.toLowerCase() == "yes" || myJob.toLowerCase() == "y") {
     alert("sorry");
     // console.log(myJob);
-}else if(myJob.toLowerCase() == "no" || myJob.toLowerCase() == "n"){
+} else if (myJob.toLowerCase() == "no" || myJob.toLowerCase() == "n") {
     alert("that's another correct answer");
     score++
 
     // console.log(myJob);
-}else{
+} else {
     alert("Enter only yes ' y or no ' n .");
 }
 // four question 
 
 let food = prompt('do i love food ?');
-if(food.toLowerCase() == "yes" || food.toLowerCase() == "y"){
+if (food.toLowerCase() == "yes" || food.toLowerCase() == "y") {
     alert("woow");
     score++
 
     // console.log(food);
-}else if(food.toLowerCase() == "no" || food.toLowerCase() == "n"){
+} else if (food.toLowerCase() == "no" || food.toLowerCase() == "n") {
     alert("everybody love food");
     // console.log(food);
-}else{
+} else {
     alert("Enter only yes ' y or no ' n .");
 }
 // five question 
 
 let witherLove = prompt('do i love winter ?');
-if(witherLove.toLowerCase() == "yes" || witherLove.toLowerCase() == "y"){
+if (witherLove.toLowerCase() == "yes" || witherLove.toLowerCase() == "y") {
     alert("Correct");
     score++
 
     // console.log(witherLove);
-}else if(witherLove.toLowerCase() == "no" || witherLove.toLowerCase() == "n"){
+} else if (witherLove.toLowerCase() == "no" || witherLove.toLowerCase() == "n") {
     alert("Wrong");
     // console.log(witherLove);
-}else{
+} else {
     alert("Enter only yes ' y or no ' n .");
 }
 
@@ -85,57 +91,57 @@ if(witherLove.toLowerCase() == "yes" || witherLove.toLowerCase() == "y"){
 
 alert('guess number between 1 and 10 ...you have 4 attempets to try');
 
-let gussed = null ;
+let gussed = null;
 
 for (let i = 0; i < 4; i++) {
-     gussed = prompt (' Guess a number from 1 to 10 : ');
-     gussed = parseInt(gussed);
+    gussed = prompt(' Guess a number from 1 to 10 : ');
+    gussed = parseInt(gussed);
 
-if (gussed >= 5 && gussed < 7 ) {
-    alert (' low but too close');
-}else if( gussed <= 9 && gussed > 7){
-    alert (' you are to close , try a smaler number ');
-}
-else if (gussed > 7 ) {
-
-    gussed= alert ('too high num ');
-}else if (gussed < 7 ) {
-
-        gussed= alert ('too low num ');
+    if (gussed >= 5 && gussed < 7) {
+        alert(' low but too close');
+    } else if (gussed <= 9 && gussed > 7) {
+        alert(' you are to close , try a smaler number ');
     }
-else if (gussed ===7 ){
-    alert ('you are in fire' )
-    break;
-     score++
-}
-else {
-    alert ('sry try again ')
-}
-if (i===3){
-    alert('correct answer 7')
-}
+    else if (gussed > 7) {
+
+        gussed = alert('too high num ');
+    } else if (gussed < 7) {
+
+        gussed = alert('too low num ');
+    }
+    else if (gussed === 7) {
+        alert('you are in fire')
+        break;
+        score++
+    }
+    else {
+        alert('sry try again ')
+    }
+    if (i === 3) {
+        alert('correct answer 7')
+    }
 }
 
 // 7 quesion 
 
-let theAnswer =['BMW' , 'MERCEDES','FERRARI','LUXES','BUGATTI','RANGEROVER','DODGE','TOYOTA',]
+let theAnswer = ['BMW', 'MERCEDES', 'FERRARI', 'LUXES', 'BUGATTI', 'RANGEROVER', 'DODGE', 'TOYOTA',]
 
 
 for (let i = 0; i < 6; i++) {
 
-        
-let userAnswer = prompt('what is your best car ?');
-userAnswer = userAnswer.toUpperCase();
- for (let j = 0; j < theAnswer.length; j++) {
-console.log(j);
- if (theAnswer[j] === userAnswer) {
- alert('you are awosem');
- score++;
-                      
- i =7;
-  break;
- }
-}
+
+    let userAnswer = prompt('what is your best car ?');
+    userAnswer = userAnswer.toUpperCase();
+    for (let j = 0; j < theAnswer.length; j++) {
+        console.log(j);
+        if (theAnswer[j] === userAnswer) {
+            alert('you are awosem');
+            score++;
+
+            i = 7;
+            break;
+        }
+    }
 }
 
 // wellcome masseg 
